5f0c0a6ad8ed96dd399491255b47db45613267c5
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-16 22:58:42 +0300

    fix: add `MEDIA_ROOT` and add localization for apps

--- django_core/django_core/settings.py
+++ django_core/django_core/settings.py
@@ -140,6 +140,10 @@ STATICFILES_FINDERS = [
 
 ]
 
+# Media files storage
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- django_core/lecture_app/apps.py
+++ django_core/lecture_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class LectureConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "lecture_app"
+    verbose_name = 'Теория'
