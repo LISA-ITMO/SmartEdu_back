46ee5880e4db9314b75192aa121da9b1e9b6392a
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-16 20:17:07 +0300

    fix: refactor theory models

--- django_core/lecture_app/models.py
+++ django_core/lecture_app/models.py
@@ -1,15 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-from djongo import models
 
-
-# Create your models here.
 
 class TheoryType(models.Model):
     title = models.CharField(max_length=1024)
-
-    class Meta:
-        abstract = False
 
 
 class Theory(models.Model):
@@ -18,34 +11,36 @@ class Theory(models.Model):
     time_to_read = models.IntegerField(default=300)  # 5 minutes
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, null=True, related_name='Author', default=1, on_delete=models.SET_NULL)
-    theory_type = models.ForeignKey(TheoryType, null=True, on_delete=models.SET_NULL)
-    theory_content = models.DjongoManager()
-
-    class Meta:
-        abstract = False
+    created_by = models.ForeignKey("user_app.User",
+                                   null=True,
+                                   related_name='theory',
+                                   default=1,
+                                   on_delete=models.SET_NULL)
+    theory_type = models.ForeignKey(TheoryType,
+                                    null=True,
+                                    on_delete=models.SET_NULL)
 
 
 class TheoryPresentContent(models.Model):
-    theory = models.ForeignKey(Theory, null=True, on_delete=models.SET_NULL)
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="files")
     file_path = models.FileField()
-
-    class Meta:
-        abstract = True
 
 
 class TheoryVideoContent(models.Model):
-    theory = models.ForeignKey(Theory, null=True, on_delete=models.SET_NULL)
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="videos")
     file_path = models.FileField()
-    url = models.TextField(max_length=255)
-
-    class Meta:
-        abstract = True
+    url = models.URLField()
 
 
 class TheoryTextContent(models.Model):
-    theory = models.ForeignKey(Theory, null=True, on_delete=models.SET_NULL)
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="text")
     content = models.TextField()
-
-    class Meta:
-        abstract = True
