ec454d06f75c759aa8427adc87f4220e0d5d3cbf
Andrei V. Berezhkov <90522407+a-berezhkov@users.noreply.github.com>
2023-12-16 20:18:02 +0300

    Make theory models (#2)
    * feat: add .DS_Store to .gitignore
    
    * feat: theory models added
    
    * fix: refactor theory models
    
    ---------
    
    Co-authored-by: Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>

--- django_core/lecture_app/models.py
+++ django_core/lecture_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class TheoryType(models.Model):
+    title = models.CharField(max_length=1024)
+
+
+class Theory(models.Model):
+    title = models.CharField(max_length=1024)
+    desc = models.TextField(null=True)
+    time_to_read = models.IntegerField(default=300)  # 5 minutes
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey("user_app.User",
+                                   null=True,
+                                   related_name='theory',
+                                   default=1,
+                                   on_delete=models.SET_NULL)
+    theory_type = models.ForeignKey(TheoryType,
+                                    null=True,
+                                    on_delete=models.SET_NULL)
+
+
+class TheoryPresentContent(models.Model):
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="files")
+    file_path = models.FileField()
+
+
+class TheoryVideoContent(models.Model):
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="videos")
+    file_path = models.FileField()
+    url = models.URLField()
+
+
+class TheoryTextContent(models.Model):
+    theory = models.ForeignKey(Theory,
+                               null=True,
+                               on_delete=models.SET_NULL,
+                               related_name="text")
+    content = models.TextField()
