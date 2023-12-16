1eb80cf20cc8c9e2ed01317be1509048c6e77687
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-16 20:25:52 +0300

    feat: add migrations and admin for lecture_app

--- django_core/lecture_app/admin.py
+++ django_core/lecture_app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Theory, TheoryTextContent, TheoryPresentContent, TheoryVideoContent, TheoryType
 
-# Register your models here.
+
+class TheoryTextContentInline(admin.TabularInline):
+    model = TheoryTextContent
+    extra = 0
+
+
+class TheoryPresentContentInline(admin.TabularInline):
+    model = TheoryPresentContent
+    extra = 0
+
+
+class TheoryVideoContentInLine(admin.TabularInline):
+    model = TheoryVideoContent
+    extra = 0
+
+
+@admin.register(Theory)
+class TheoryAdmin(admin.ModelAdmin):
+    inlines = [
+        TheoryTextContentInline,
+        TheoryPresentContentInline,
+        TheoryVideoContentInLine
+    ]
+
+
+@admin.register(TheoryType)
+class TheoryTypeAdmin(admin.ModelAdmin):
+    pass
