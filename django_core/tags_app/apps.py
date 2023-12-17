bfb6e82ca425d2932e2a5e3a469932ce46af7100
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-17 19:52:57 +0300

    fix: add verbose name for `tags_app`

--- django_core/tags_app/apps.py
+++ django_core/tags_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TagsAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tags_app"
+    verbose_name = "Создание тэгов"
