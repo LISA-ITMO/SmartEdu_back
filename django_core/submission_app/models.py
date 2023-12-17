439ff3119964bfb67380deee2089e6cd822313d0
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-17 16:41:16 +0300

    feat: move `Submission` model to `submission_app`

--- django_core/submission_app/models.py
+++ django_core/submission_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from .enums import StatusEnum, LanguageEnum
 
-# Create your models here.
+
+class Submission(models.Model):
+
+    """Model that represent submission and checking task"""
+
+    user = models.ForeignKey(
+        "user_app.User", on_delete=models.SET_NULL, related_name="submissions"
+    )
+
+    task = models.ForeignKey(
+        "task_app.Task", on_delete=models.SET_NULL, related_name="submissions"
+    )
+
+    language = models.TextField(
+        verbose_name="Язык программирования",
+        max_length=64,
+        choices=LanguageEnum.choices,
+    )
+
+    code = models.TextField(blank=True)
+
+    status = models.CharField(
+        verbose_name="Тип пользователя",
+        max_length=64,
+        choices=StatusEnum.choices,
+        default=StatusEnum.PENDING,
+    )
+
+    worker_uuid = models.CharField(max_length=1024, verbose_name="Номер задачи")
--- django_core/task_app/models.py
+++ django_core/task_app/models.py
@@ -1,27 +1,2 @@
 from django.db import models
-from .enums import StatusEnum, LanguageEnum
 
-
-class Submission(models.Model):
-
-    """Model that represent submission and checking task"""
-
-    user = models.ForeignKey("user_app.User",
-                             on_delete=models.SET_NULL,
-                             related_name="submissions")
-
-    task = None
-
-    language = models.TextField(verbose_name="Язык программирования",
-                                max_length=64,
-                                choices=LanguageEnum.choices,)
-
-    code = models.TextField(blank=True)
-
-    status = models.models.CharField(verbose_name="Тип пользователя",
-                                     max_length=64,
-                                     choices=StatusEnum.choices,
-                                     default=StatusEnum.PENDING)
-
-    worker_uuid = models.CharField(max_length=1024,
-                                   verbose_name="Номер задачи")
