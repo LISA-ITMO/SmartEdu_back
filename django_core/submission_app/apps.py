f0012d49da5563bc7f28d1c7306f4c18b4372f13
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2023-12-17 18:51:01 +0300

    feat: Add verbose names for apps

--- django_core/submission_app/apps.py
+++ django_core/submission_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SubmissionAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "submission_app"
+    verbose_name = "Тесты задач"
