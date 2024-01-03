21e24c9c943e190986bbfd2b29ff871b2fa1989a
eeyunghefner <kravnik.03@gmail.com>
2024-01-03 20:23:44 +0300

    feat: add GET, POST, PUT, PATCH and DELETE-requests

--- django_core/task_app/urls.py
+++ django_core/task_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from .views import (
-    TaskListView,
-    TaskTypeListView,
     CodeTaskRetrieveView,
     TestCaseListView,
+    TaskListViewSet,
+    TaskTypeListViewSet,
 )
 
 urlpatterns = [
-    path("tasks", TaskListView.as_view()),
+    path("tasks", TaskListViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("tasks/<int:task>", TaskListViewSet.as_view({'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})),
     path("tasks/<int:task>/code-tasks", CodeTaskRetrieveView.as_view()),
-    path("tasks/types", TaskTypeListView.as_view()),
-    path("tasks/code-tasks/<int:code_task>/testcases", TestCaseListView.as_view()),
+    path("tasks/types", TaskTypeListViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path("tasks/code-tasks/<int:code_task>/testcases", TestCaseListView.as_view())
 ]
