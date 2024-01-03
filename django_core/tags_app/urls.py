3d1c5d288a24840584e7e2ed17bc3b04ad8a799b
eeyunghefner <kravnik.03@gmail.com>
2024-01-03 20:19:49 +0300

    fix: change ListAPIView to ModelViewSet

--- django_core/tags_app/urls.py
+++ django_core/tags_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import TagsListView
+from .views import TagsListViewSet
 
 urlpatterns = [
-    path("tags/", TagsListView.as_view())
+    path("tags/", TagsListViewSet.as_view({"get": "list", "post": "create"}))
 ]
--- django_core/tags_app/views.py
+++ django_core/tags_app/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Tag
 from .serializers import TagSerializer
 
 
-class TagsListView(ListAPIView):
+class TagsListViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
