78a90fc791890e49462ce05db9bf03fa8e17249c
eeyunghefner <kravnik.03@gmail.com>
2024-01-07 00:58:25 +0300

    fix: remove path

--- django_core/task_app/urls.py
+++ django_core/task_app/urls.py
@@ -13,8 +13,8 @@ router.register(r'tasks', TaskListViewSet)
 
 
 urlpatterns = [
-    path("", include(router.urls)),
     path("tasks/<int:task>/code-tasks", CodeTaskRetrieveView.as_view()),
     path("tasks/types", TaskTypeListView.as_view()),
     path("tasks/code-tasks/<int:code_task>/testcases", TestCaseListView.as_view())
 ]
+urlpatterns += router.urls
